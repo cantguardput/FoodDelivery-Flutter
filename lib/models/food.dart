6d132739380c41543c5d71d9c943957475e0cfa8
cantguardput <puruardika@gmail.com>
2024-06-29 22:01:40 +0700

    component, modesl Homepage not fix

---
+++ lib/models/food.dart
@@ -0,0 +1,36 @@
+class Food{
+  final String name;
+  final String description;
+  final String imagePath;
+  final String price;
+  final FoodCategory category;
+  List<Addon>availableAddons;
+
+  Food({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.price,
+    required this.category,
+    required this.availableAddons,
+
+});
+}
+
+  enum FoodCategory{
+    burgers,
+    Salads,
+    sides,
+    desert,
+    drinks,
+  }
+
+  class Addon{
+    String name;
+    double price;
+
+    Addon({
+      required this.name,
+      required this.price,
+  });
+}
--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fooddelevery/components/my_current_location.dart';
+import 'package:fooddelevery/components/my_description_box.dart';
 import 'package:fooddelevery/components/my_drawer.dart';
+import 'package:fooddelevery/components/my_sillver_app_bar.dart';
+import 'package:fooddelevery/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,15 +11,62 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage>createState() =>_HomePageState();
 }
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
-class _HomePageState extends State<HomePage> {
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text ("Home"),
-      backgroundColor: Theme.of(context).colorScheme.background,
-    ),
       drawer: MyDrawer(),
+      body:  NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            title: MyTabBar(tabController: _tabController ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                // my location
+                const MyCurrentLocation(),
+                // box describe
+                const MyDescriptionBox(),
+              ],
+            ),
+          ),
+      ],
+          body: TabBarView(
+        controller:  _tabController,
+        children: [
+          ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) => Text("first Item"),
+          ),
+
+          ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) => Text("first Item"),
+          ),
+          ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) => Text("first Item"),
+          ),
+        ],
+      )
+      ),
     );
   }
 }
